95907e29d51c53149cbcb2872054a72baddaf5e1
David Cárdenas <davidcardenasorozco@gmail.com>
2023-06-13 14:33:18 -0600

    Initial Commit

---
+++ hotelapp-frontend/hotelapp-frontend/Controllers/AboutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace hotelapp_frontend.Controllers
+{
+    public class AboutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ hotelapp-frontend/hotelapp-frontend/Controllers/RoomController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace hotelapp_frontend.Controllers
+{
+    public class RoomController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
