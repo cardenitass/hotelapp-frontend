509a3f8d528bcdac23cef9a55874684c8f95cfd3
David Cárdenas <davidcardenasorozco@gmail.com>
2023-07-18 21:10:51 -0600

    Login Page

---
+++ hotelapp-frontend/hotelapp-frontend/Controllers/UserController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace hotelapp_frontend.Controllers
+{
+    public class UserController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
